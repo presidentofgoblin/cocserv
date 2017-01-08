d76c40f64406b6316b55381cbc3a55561e0b5ef6
xXAlphaManXx <smartclashcoc@gmail.com>
2017-01-08 17:04:02 +0530

    Fix the logout problem

--- application/controllers/Auth.php
+++ application/controllers/Auth.php
@@ -20,7 +20,8 @@ class Auth extends CI_Controller
 
     public function logout()
     {
-
+        $this->session->sess_destroy();
+        redirect(base_url());
     }
 
     public function register()
